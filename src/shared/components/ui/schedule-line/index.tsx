100439bdb043fefff4d173a0837954c7bc16f8bf
kevinlobster <vladmarket183@gmail.com>
2024-12-30 19:48:43 +0200

    fix micro

--- src/shared/components/ui/schedule-line/index.tsx
+++ src/shared/components/ui/schedule-line/index.tsx
@@ -1,14 +1,14 @@
-import {FC, useState} from "react";
-import S from './style.module.scss';
-import {Modal} from "antd";
+import { FC, useState } from "react";
+import style from './style.module.scss'
+import { Modal } from "antd";
 import Button from "@/shared/components/ui/button";
 
 export const ScheduleLine: FC = () => {
   const [isModal, setIsModal] = useState(false);
 
   return (
-    <div className={S.container}>
-      <Button onClick={() => setIsModal(true)}>Просмотреть время работы</Button>
+    <div className={style.container}>
+      <Button onClick={() => setIsModal(true)}>Переглянути час роботи на свята ⌚</Button>
       <Modal open={isModal} onCancel={() => setIsModal(false)} footer={null}>
         <h2>Графік роботи</h2>
         <p>

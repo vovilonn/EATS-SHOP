56cdad39ef39c218dde9e4850da985f7057a5e6b
Sergey Beltsin <beltsins89@gmail.com>
2024-12-31 16:26:43 +0300

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ const Document = () => {
   return (
     <Html lang='en'>
       <Head>
+        <title>EATS - доставка їжі Мукачево</title>
+        <meta name="description" content="Замовляй піцу, роли, боули та багато іншого! Швидка доставка з 10:00 до 22:00. +380990005758" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width"></meta>
         <meta name="facebook-domain-verification" content="wz3yhfxrby0lq81p2w75pwvg5jmzux" />
         <FacebookPixelEvents />
